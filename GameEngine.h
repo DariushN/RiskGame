480bf45f1ba8d09c2aae94ed6c2a221a7db7d988
Mackenzie Bellemore <m.bellemore@hotmail.com>
2019-11-04 22:23:02 -0500

    Putting back my changes

--- GameEngine.h
+++ GameEngine.h
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 /* 
  * File:   GameEngine.h
@@ -18,21 +23,21 @@ using std::string;
 
 class GameEngine {
 public:
-    GameEngine(); // Default constructor
-    GameEngine(const GameEngine& orig); // Copy constructor
-    virtual ~GameEngine(); // Destructor
-    void SelectMaps(); // Allow the user to select a map
-    void Setup(); // Set up players and territories
+    GameEngine();
+    GameEngine(const GameEngine& orig);
+    virtual ~GameEngine();
+    void SelectMaps();
+    void Setup();
     void printTerritories();
-    bool compliesWithA2Q2(); // Check if game was set up correctly
-    void mainGameLoop(); // Allow a player to make a move (reinforce, attack, fortify)
-    void mainGameLoop(Map* MAP, std::vector<Player*> players); // Overloaded game loop
+    bool compliesWithA2Q2();
+    void mainGameLoop();
+    void mainGameLoop(Map* MAP, std::vector<Player*> players);
 private:
-    Map* MAP; // User-selected map
-    int N_players; // Number of players
-    Player* players; // Array of players
+    Map* MAP;
+    int N_players;
+    Player* players;
     Deck* deck;
-    vector<string> get_all_files_names_within_folder(string folder); // Vector of file names within directory
+    vector<string> get_all_files_names_within_folder(string folder);
     
     
 };
--- GamePlayStartupDriver.cpp
+++ GamePlayStartupDriver.cpp
@@ -11,13 +11,10 @@ using std::to_string;
 
 
 int main(int argc, char** argv) {
-	
-    GameEngine GE; // Declare GameEngine object
-    GE.SelectMaps(); // Allow the user to select a map
-    GE.Setup(); // Set up players and territories
+    GameEngine GE;
+    GE.SelectMaps();
+    GE.Setup();
     GE.printTerritories();
-	
-	// Check if country and army assignment are correct
     if(GE.compliesWithA2Q2()){
         cout<<"Game complies with rules as laid out in the requirements of part #2!"<<endl;
     }else{
