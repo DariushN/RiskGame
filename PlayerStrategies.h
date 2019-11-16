d218838a23dff18ff5b2dbe86a5ac3615acc2088
DariushN <35940376+DariushN@users.noreply.github.com>
2019-11-16 18:59:47 -0500

    Implemented attack strategies and finished player driver

--- PlayerStrategies.h
+++ PlayerStrategies.h
@@ -2,6 +2,7 @@
 #include "Player.h"
 #include <cstdlib>
 #include <iostream>
+
 class Player;
 
 #ifndef SRC_PLAYERSTRATEGIES_H_
@@ -9,41 +10,42 @@ class Player;
 
 class PlayerStrategies {
 public:
-	virtual ~PlayerStrategies() = 0;
-	virtual void reinforce(Map* map, Player* player) = 0;
-	//virtual void attack(Map* map) = 0;
-	virtual void fortify(Player* player) = 0;
+    virtual ~PlayerStrategies() = 0;
+    virtual void reinforce(Map* map, Player* player) = 0;
+    virtual void attack(Map* map, Player* player) = 0;
+    virtual void fortify(Player* player) = 0;
 };
 
 class HumanPlayer : public PlayerStrategies {
 public:
-	void reinforce(Map* map, Player* player);
-	//void attack(Map* map);
-	void fortify(Player* player);
-	~HumanPlayer(){
-		std::cout << "human player destructor \n";
-	}
+    void reinforce(Map* map, Player* player);
+    void attack(Map* map, Player* player);
+    void fortify(Player* player);
+    ~HumanPlayer(){
+        std::cout << "human player destructor \n";
+    }
 };
 
 class AggressiveComputer : public PlayerStrategies {
 public:
-	void reinforce(Map* map, Player* player);
-	//void attack(Map* map);
-	void fortify(Player* player);
-	~AggressiveComputer(){
-		std::cout << "Aggressive Computer destructor \n";
-	}
+    void reinforce(Map* map, Player* player);
+    void attack(Map* map, Player* player);
+    void fortify(Player* player);
+    ~AggressiveComputer(){
+        std::cout << "Aggressive Computer destructor \n";
+    }
 };
 
 class BenevolentComputer : public PlayerStrategies {
-	void reinforce(Map* map, Player* player);
-		//void attack(Map* map);
-		void fortify(Player* player);
-		~BenevolentComputer(){
-			std::cout << "Benevolent Computer destructor \n";
-		}
+    void reinforce(Map* map, Player* player);
+    void attack(Map* map, Player* player);
+    void fortify(Player* player);
+    ~BenevolentComputer(){
+        std::cout << "Benevolent Computer destructor \n";
+    }
 };
 
+int maxDiceToRoll(bool isAttacker, Territory *territory);
 
 #endif /* SRC_PLAYERSTRATEGIES_H_ */
 
--- PlayerStrategiesDriver.cpp
+++ PlayerStrategiesDriver.cpp
@@ -12,28 +12,43 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	//get a map for demonstration purposes
-	Map* MAP = MapLoader::MapBuilder("Maps/risk.map");
+	Map* MAP = MapLoader::MapBuilder("../Maps/risk.map");
 	std::cout<<"Map name: " << MAP->getName() << endl;
 
 
 
 	//Initialize some player objects
-	std::string name1 = "Player1";
-	Player* player = new Player();
-	player->setName(name1);
+    std::string name1 = "AggressivePlayer";
+    Player* player = new Player();
+    player->setName(name1);
 	std::cout << "Player Name: " << player->getName() << std::endl;
+    //Initialize some player objects
+    std::string name2= "HumanPlayer";
+    Player* player2 = new Player();
+    player2->setName(name2);
+    std::cout << "Player Name: " << player2->getName() << std::endl;
+    //Initialize some player objects
+    std::string name3 = "BenevolantPlayer";
+    Player* player3 = new Player();
+    player3->setName(name3);
+    std::cout << "Player Name: " << player3->getName() << std::endl;
 
 	//give player some initial territories
-	for(int i=0; i<4; i++) {
+	for(int i=0; i<10; i++) {
 		MAP->Territories[i]->setOwner(player);
 		MAP->Territories[i]->incTroops(i);
 		player->lands.push_back(MAP->Territories[i]);
 	}
-	for(int i=20; i<25; i++) {
-		MAP->Territories[i]->setOwner(player);
+	for(int i=10; i<30; i++) {
+		MAP->Territories[i]->setOwner(player2);
 		MAP->Territories[i]->incTroops(i);
-		player->lands.push_back(MAP->Territories[i]);
+		player2->lands.push_back(MAP->Territories[i]);
 	}
+    for(int i=30; i<42; i++) {
+        MAP->Territories[i]->setOwner(player3);
+        MAP->Territories[i]->incTroops(i);
+        player3->lands.push_back(MAP->Territories[i]);
+    }
 
 	//player owns a hand of risk cards
 	Deck* deck = new Deck(MAP->Territories);
@@ -44,13 +59,46 @@ int main(int argc, char** argv) {
 	deck->draw(player->getHand());
 	deck->draw(player->getHand());
 
-	player->setStrategy(new HumanPlayer());
-	player->reinforce(MAP);
-	std::cout << "\n\n";
+    deck->draw(player2->getHand());
+    deck->draw(player2->getHand());
+    deck->draw(player2->getHand());
+    deck->draw(player2->getHand());
+    deck->draw(player2->getHand());
+    deck->draw(player2->getHand());
+
+    deck->draw(player3->getHand());
+    deck->draw(player3->getHand());
+    deck->draw(player3->getHand());
+    deck->draw(player3->getHand());
+    deck->draw(player3->getHand());
+    deck->draw(player3->getHand());
+
+    // Setting strategies
 	player->setStrategy(new AggressiveComputer());
+	player2->setStrategy(new HumanPlayer());
+	player3->setStrategy(new BenevolentComputer());
 	player->reinforce(MAP);
-	std::cout << "\n\n";
-	player->setStrategy(new BenevolentComputer());
-	player->reinforce(MAP);
-	std::cout << "\n\n";
+    std::cout << "\n\n\n\n";
+    player2->reinforce(MAP);
+    std::cout << "\n\n\n\n";
+    player3->reinforce(MAP);
+    std::cout << "\n\n\n\n";
+    player->attack(MAP);
+    std::cout << "\n\n\n\n";
+
+    //Changing strategy mid-play (switching strategy of player 2 and 3)
+    player2->setStrategy(new BenevolentComputer());
+    player3->setStrategy(new HumanPlayer());
+
+    player2->attack(MAP);
+    std::cout << "\n\n\n\n";
+    player3->attack(MAP);
+    std::cout << "\n\n\n\n";
+    player->fortify();
+    std::cout << "\n\n\n\n";
+    player2->fortify();
+    std::cout << "\n\n\n\n";
+    player3->fortify();
+
+
 }
